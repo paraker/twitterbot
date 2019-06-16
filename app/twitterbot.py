e887e3f4fe3d27a0b8b48dca42b45e6e6e8bbf69
Par Akerstrom <par.w.akerstrom@gmail.com>
2019-06-16 16:00:06 +1000

    Add of twitter authentication

--- app/twitterbot.py
+++ app/twitterbot.py
@@ -1,5 +1,27 @@
 import tweepy
+import json
 
-with open()
+with open('keys.json', 'r') as keys:
+    keys = json.load(keys)
+
 # Auth to twitter
-auth = tweepy.OAuthHandler()
+auth = tweepy.OAuthHandler(
+    keys['consumer_api_key'],
+    keys['consumer_api_secret']
+)
+auth.set_access_token(
+    keys['access_token'],
+    keys['access_secret']
+)
+
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication successful")
+except BaseException as e:
+    print(f"Error during auth: {e}")
+
+
+# if __name__ == '__main__':
+#
