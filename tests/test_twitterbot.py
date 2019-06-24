cfc6421e04396bb7a18e83ea540f901ac8b316ce
Par Akerstrom <par.w.akerstrom@gmail.com>
2019-06-24 20:46:32 +1000

    changed tests

--- tests/test_twitterbot.py
+++ tests/test_twitterbot.py
@@ -12,8 +12,7 @@ def test_twitterbot(bot):
 
 
 def test_twitter_auth(bot):
-    with pytest.raises(TypeError):
-        bot.twitter_auth()
+    bot.twitter_auth()
 
 
 def test_create_api_object(bot):
